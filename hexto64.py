f51bf29ba0a6f56bc896d247a70d3e092a110309
Audiodrome <lien.daniel.h@gmail.com>
2016-04-19 21:29:21 -0700

    save

--- hexto64.py
+++ hexto64.py
@@ -5,9 +5,9 @@ hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6
 
 unHex = binascii.unhexlify(hex)
 
-print hex_str
+print unHex
 
-base_sixtyfour = base64.b64encode(hex_str)
+base_sixtyfour = base64.b64encode(unHex)
 
 print base_sixtyfour
 
