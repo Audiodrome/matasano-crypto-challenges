aade94910f27ad3cabbdef77799082c546cce15c
Audiodrome <lien.daniel.h@gmail.com>
2016-04-19 21:28:54 -0700

    save

--- hexto64.py
+++ hexto64.py
@@ -3,7 +3,7 @@ import base64
 
 hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-hex_str = binascii.a2b_hex(hex)
+unHex = binascii.unhexlify(hex)
 
 print hex_str
 
