ab66bdbcb446d99bbaee1bad3ccf31890936f8c8
Audiodrome <lien.daniel.h@gmail.com>
2016-05-07 21:58:40 -0700

    save

--- set1/3-singlebyteXORcipher.py
+++ set1/3-singlebyteXORcipher.py
@@ -8,9 +8,8 @@ hexEncodeStr = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b
 hexEncodeStr = binascii.unhexlify(hexEncodeStr)
 s1 = int.from_bytes(hexEncodeStr, byteorder = sys.byteorder)
 
-
 for i in string.ascii_letters:
-    #print (i)
+    # print (i)
     alpha = i*len(hexEncodeStr)
     print (bytes(alpha, 'ascii'))
     alpha = bytes(alpha, 'ascii')
@@ -18,28 +17,19 @@ for i in string.ascii_letters:
     #print (alpha)
     # result = base64.b16decode(alpha)
     s2 = int.from_bytes(alpha, byteorder = sys.byteorder)
-    # print ("S1: %s" % s1)
-    # print ("S2: %s" % s2)
-    #print (s1)
-    #print (s2)
+
     xord = s1 ^ s2
     xord = xord.to_bytes((xord.bit_length() // 8) + 1,
                           byteorder = sys.byteorder)
     print (xord)
-    #result = base64.b16encode(xord)
-    #print (result)
+    # Do not need to encode - deciphering
+    # result = base64.b16encode(xord)
+    # print (result)
 
-# import string
-
+# The following is  heavyberry's solution.
+# http://perso.heavyberry.com/writeups/cryptopals1.html
 # h = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 # hh = bytes.fromhex(h)
-
-# for i in string.ascii_letters:
-#     print(type(i*len(hh)))
-
-# h = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-# hh = bytes.fromhex(h)
-    
 # for k in string.ascii_letters:
 #     print(k)
 #     print(bytes([a^b for (a,b) in zip(hh,bytes(k*len(hh),'ascii'))]))
