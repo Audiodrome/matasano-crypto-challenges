63b75ca13989ab7ada5545b09dfefb3c25cb1666
Audiodrome <Audiodrome@users.noreply.github.com>
2016-05-07 22:02:41 -0700

    Update 3-singlebyteXORcipher.py

--- set1/3-singlebyteXORcipher.py
+++ set1/3-singlebyteXORcipher.py
@@ -9,24 +9,22 @@ hexEncodeStr = binascii.unhexlify(hexEncodeStr)
 s1 = int.from_bytes(hexEncodeStr, byteorder = sys.byteorder)
 
 for i in string.ascii_letters:
-    # print (i)
     alpha = i*len(hexEncodeStr)
     print (bytes(alpha, 'ascii'))
     alpha = bytes(alpha, 'ascii')
-    #alpha = binascii.unhexlify(alpha)
-    #print (alpha)
-    # result = base64.b16decode(alpha)
+
     s2 = int.from_bytes(alpha, byteorder = sys.byteorder)
 
     xord = s1 ^ s2
     xord = xord.to_bytes((xord.bit_length() // 8) + 1,
                           byteorder = sys.byteorder)
     print (xord)
+    
     # Do not need to encode - deciphering
     # result = base64.b16encode(xord)
     # print (result)
 
-# The following is  heavyberry's solution.
+# The following is  heavyberry's solution. Got a lot of help from him.
 # http://perso.heavyberry.com/writeups/cryptopals1.html
 # h = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 # hh = bytes.fromhex(h)
